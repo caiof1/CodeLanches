e91535bbed34e95d205c340ffe3dc52fdb440ad1
Caio Fernandes <caionl2071@gmail.com>
2023-06-06 18:25:15 -0300

    Home criada e finalizada

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,7 +1,36 @@
+import styles from './Home.module.css'
 
 const Home = () => {
+
   return (
-    <div>Home</div>
+    <div>
+      <section className={styles.container}>
+        <div>
+          <p>Clique no botão para criar pedidos</p>
+          <button className='btn'>Fazer pedido</button>
+        </div>
+        <div>
+          <p>Aqui pode ver todos os pedidos</p>
+          <button className='btn'>Ver pedidos</button>
+        </div>
+        <div>
+          <p>Adicionar produtos ao cardápio</p>
+          <button className='btn'>Produtos</button>
+        </div>
+        <div>
+          <p>Adicionar novas categorias</p>
+          <button className='btn'>Categorias</button>
+        </div>
+        <div>
+          <p>Adicionar novos colaboradores</p>
+          <button className='btn'>Colaboradores</button>
+        </div>
+        <div>
+          <p>Ver o dashboard</p>
+          <button className='btn'>Dashboard</button>
+        </div>
+      </section>
+    </div>
   )
 }
 
